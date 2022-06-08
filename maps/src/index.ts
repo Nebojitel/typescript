ef9e7273609860f92f4605a8994c087d7466172c
Alexey Kuznetsov <alexal.kuznetsov@mail.ru>
2022-06-08 16:05:54 +0300

    finished

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,1 +1,15 @@
+/// <reference types="@types/google.maps" />
 import { User } from './User';
+import { Company } from './Company';
+import { CustomMap } from './CustomMap';
+
+const customMap = new CustomMap('map');
+
+const user = new User();
+customMap.addMarker(user);
+
+const company = new Company();
+customMap.addMarker(company);
+
+// console.log(user);
+// console.log(company);
